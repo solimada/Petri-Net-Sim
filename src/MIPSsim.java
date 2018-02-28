23a66ee6be786f5ea903035c4206bb326251b76b
Adam Soliman <solimada41@gmail.com>
2018-02-28 14:51:17 -0500

    added classes for token types

--- src/MIPSsim.java
+++ src/MIPSsim.java
@@ -1,12 +1,22 @@
-
-
-
-
+import java.util.*;
 
 public class MIPSsim {
 
+    public static final String REGISTERFILE = "registers.txt";
+    public static final String DATAFILE = "datamemory.txt";
+    public static final String INSTRUCTIONFILE = "instructions.txt";
+
+    private List<InstructionToken> instructionMemory = new ArrayList<InstructionToken>();
+    private List<RegisterToken> registerFile = new ArrayList<RegisterToken>();
+    private List<AddressToken> dataMemory = new ArrayList<AddressToken>();
+
+
+    public MIPSsim(){
+
+    }
 
     public void readAndDecode(){
+
     }
     public void Issue(){
 
@@ -28,6 +38,49 @@ public class MIPSsim {
     }
     public void write(){
 
+    }
+}
+
+enum Instruction {ADD, SUB, MUL, ST}
+enum Register {R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15}
+
+class InstructionToken{
+    Instruction opcode;
+    Register destination;
+    Register source1;
+    String source2;
+    boolean isStore = false;
+
+    public InstructionToken(String token){
+        String temp [] = token.replace("<","").replace(">","").split(",");
+        opcode = Instruction.valueOf(temp[0]);
+        destination = Register.valueOf(temp[1]);
+        source1 = Register.valueOf(temp[2]);
+        source2 = temp[3];
+        if(opcode == Instruction.ST){
+            isStore = true;
+        }
+    }
+
+}
+
+class RegisterToken{
+    Register registerName;
+    int value;
+    public RegisterToken(String token){
+        String temp [] = token.replace("<","").replace(">","").split(",");
+        registerName = Register.valueOf(temp[0]);
+        value = Integer.parseInt(temp[1]);
+    }
+}
+
+class AddressToken{
+    int address;
+    int value;
+    public AddressToken(String token){
+        String temp [] = token.replace("<","").replace(">","").split(",");
+        address = Integer.parseInt(temp[0]);
+        value = Integer.parseInt(temp[1]);
     }
 }
 
