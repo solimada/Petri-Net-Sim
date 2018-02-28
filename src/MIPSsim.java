50b23b09398826a2083b0c860cc05908fcc42880
Adam Soliman <solimada41@gmail.com>
2018-02-28 13:59:51 -0500

    first commit

---
+++ src/MIPSsim.java
@@ -0,0 +1,33 @@
+
+
+
+
+
+public class MIPSsim {
+
+
+    public void readAndDecode(){
+    }
+    public void Issue(){
+
+    }
+    public void asu(){
+
+    }
+    public void mult1(){
+
+    }
+    public void mult2(){
+
+    }
+    public void addr(){
+
+    }
+    public void store(){
+
+    }
+    public void write(){
+
+    }
+}
+
